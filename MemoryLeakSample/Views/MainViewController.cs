3d71e323f50a270667c60bbda89a8c1761a5e6a4
Tomohiro Suzuki <suzuchan@muf.biglobe.ne.jp>
2019-08-11 15:12:18 +0900

    Add View and button

---
+++ MemoryLeakSample/Views/MainViewController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace MemoryLeakSample.Views
+{
+    public partial class MainViewController : UIViewController
+    {
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            InitializeUI();
+
+            //searchFromZipcodeButton.AddTarget(this, new ObjCRuntime.Selector("zipCodeEvent:"), UIControlEvent.TouchUpInside);
+        }
+
+    }
+}
+
---
+++ MemoryLeakSample/Views/MainViewController.designer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreGraphics;
+using UIKit;
+
+namespace MemoryLeakSample.Views
+{
+    public partial class MainViewController
+    {
+        static readonly nfloat fontSize = 20f;
+
+        UIButton button;
+
+        void InitializeUI()
+        {
+            View.ContentMode = UIViewContentMode.ScaleToFill;
+            View.LayoutMargins = new UIEdgeInsets(0, 16, 0, 16);
+            View.Frame = new CGRect(0, 0, 375, 667);
+            View.BackgroundColor = UIColor.White;
+            View.AutoresizingMask = UIViewAutoresizing.FlexibleWidth
+                                    | UIViewAutoresizing.FlexibleHeight;
+
+            button = new UIButton(UIButtonType.RoundedRect)
+            {
+                Frame = new CGRect(0, 0, 375, 20),
+                Opaque = false,
+                ContentMode = UIViewContentMode.ScaleToFill,
+                HorizontalAlignment = UIControlContentHorizontalAlignment.Center,
+                VerticalAlignment = UIControlContentVerticalAlignment.Center,
+                LineBreakMode = UILineBreakMode.MiddleTruncation,
+                TranslatesAutoresizingMaskIntoConstraints = false,
+                Font = UIFont.SystemFontOfSize(fontSize),
+                AccessibilityIdentifier = "button",
+            };
+
+            button.SetTitle("アラート表示", UIControlState.Normal);
+            View.AddSubview(button);
+
+            button.HeightAnchor.ConstraintEqualTo(40f).Active = true;
+            button.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
+            button.CenterYAnchor.ConstraintEqualTo(View.CenterYAnchor).Active = true;
+
+            button.LeftAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.LeftAnchor).Active = true;
+            button.RightAnchor.ConstraintEqualTo(View.LayoutMarginsGuide.RightAnchor).Active = true;
+        }
+    }
+}
