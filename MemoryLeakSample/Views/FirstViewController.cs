98cfebaac9ddef4449c7a828d6d287499d2273d4
Tomohiro Suzuki <suzuchan@muf.biglobe.ne.jp>
2019-08-12 16:56:33 +0900

    [wip] Creating views.

---
+++ MemoryLeakSample/Views/FirstViewController.cs
@@ -0,0 +1,34 @@
+﻿using UIKit;
+
+namespace MemoryLeakSample.Views
+{
+    public partial class MainViewController : UIViewController
+    {
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            InitializeUI();
+
+            button.TouchUpInside += (s, e) => PresentAlert("Alert");
+
+            //button.AddTarget(this, new ObjCRuntime.Selector("buttonEvent:"), UIControlEvent.TouchUpInside);
+        }
+
+        //[Export("buttonEvent:")]
+        //void ButtonEvent(NSObject sender)
+        //{
+        //    PresentAlert("Alert");
+        //}
+
+        void PresentAlert(string message)
+        {
+            var alertController = UIAlertController.Create(string.Empty, message, UIAlertControllerStyle.Alert);
+            alertController.AddAction(UIAlertAction.Create("Close", UIAlertActionStyle.Cancel, null));
+            PresentViewController(alertController, true, null);
+        }
+
+    }
+}
+
