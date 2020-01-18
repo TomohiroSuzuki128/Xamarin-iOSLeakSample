2d4fda7515d93e5574f68c8b554d31476ee9a985
Tomohiro Suzuki <suzuki@frog-pod.com>
2020-01-18 23:53:59 +0900

    Add Deep Dive05 Project

---
+++ DeepDive05/AppDelegate.cs
@@ -0,0 +1,32 @@
+﻿using Foundation;
+using UIKit;
+using DeepDive05.Views;
+
+namespace DeepDive05
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+
+        public override UIWindow Window
+        {
+            get;
+            set;
+        }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            Window = new UIWindow(UIScreen.MainScreen.Bounds)
+            {
+                RootViewController = new UINavigationController(new FirstViewController()),
+            };
+            Window.MakeKeyAndVisible();
+
+            return true;
+        }
+
+    }
+}
+
